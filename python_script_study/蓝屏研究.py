3cdda5406207351073b16f66e6a8a9f685911270
yandifei <3058439878@qq.com>
2025-03-02 22:32:59 +0800

    蓝屏开发

--- python_script_study/蓝屏研究.py
+++ python_script_study/蓝屏研究.py
@@ -1,15 +1,13 @@
 # 路径替换为 NotMyFault.exe 的实际路径
 import subprocess
-"""因为协议问题我这里就不用官网的了"""
+"""因为协议（法律）问题我这里就不用微软官方的方法了"""
 # path = r".\NotMyFault\notmyfault.exe"
 # # 调用 NotMyFault 触发蓝屏
 # subprocess.run([path, "/crash"])
 
-
 import ctypes
-# 加载 ntdll.dll
-ntdll = ctypes.WinDLL('ntdll.dll')
-# 提升权限
-ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool(0)))
-# 触发蓝屏
-ntdll.NtRaiseHardError(0xC000021A, 0, 0, 0, 6, ctypes.byref(ctypes.c_ulong(0)))
+ntdll = ctypes.WinDLL('ntdll.dll')  # 加载 ntdll.dll
+ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref (ctypes.c_bool(0)))  # 提升权限
+ntdll.NtRaiseHardError(0xC000021A, 0, 0, 0, 6, ctypes.byref(ctypes.c_ulong(0))) # 触发蓝屏
+# 怎么让蓝屏改为用户手动重启呢？以后思考
+pass
