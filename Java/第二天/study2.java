127291700c80e00bee1c3ab495170e6b85efa71e
yandifei <3058439878@qq.com>
2025-03-06 01:07:34 +0800

    java学习

---
+++ Java/第二天/study2.java
@@ -0,0 +1,19 @@
+//有多少个类就生成多少个class文件（字节码文件）
+
+public class study2{
+	public static void main(String[] args) {
+		System.out.println("主要的类");
+	}
+}
+
+class cat {
+	public static void main(String[] args) {
+		System.out.println("我是小猫");	
+	}
+}
+
+class dog {
+	public static void main(String[] args) {
+		System.out.println("我是小狗");
+	}
+}
