d6e0830002bb712915e59c8d6bf2aed096998b18
yandifei <3058439878@qq.com>
2025-03-06 01:07:34 +0800

    java学习

---
+++ Java/第二天/EscapeCharacter.java
@@ -0,0 +1,20 @@
+public class EscapeCharacter {
+	public static void main(String[] args) {
+		// System.out.println("转义字符的学习");
+		// System.out.println("你\t我\n他");
+		// System.out.println("你\\我\\\\他");
+		// System.out.println("你:\"我他\"");
+		// System.out.println("你:'我他'");
+		// System.out.println("你\r\n我他");
+		System.out.println("书名\t作者\t价格\t销量");
+		System.out.println("三国\t罗贯中\t120\t1000");
+
+	}
+}
+// 在控制台，输入tab键，可以实现命令补全
+// t:一个制表位，实现对齐的功能
+// \n:换行符
+// \\:一个\
+// \":一个"
+// \':一个'
+// \r:一个回车 System.out.println("你好\r北京”);
