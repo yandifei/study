baa735ef45ee3ad857a88d3267cbf8c2f907383d
yandifei <3058439878@qq.com>
2025-03-07 00:36:59 +0800

    字符串细节

---
+++ Java/第三天/Homework.java
@@ -0,0 +1,6 @@
+public class Homework {
+	public static void main(String[] args) {
+		System.out.println("\\n \\t \\r \\\\ 1 2 3");
+		// 要注意的是在java中，转义字符必须在字符串里面使用，如："/n",而不能 /n 直接使用
+	}
+} 
--- Java/第二天/EscapeCharacter.java
+++ Java/第二天/EscapeCharacter.java
@@ -1,13 +1,13 @@
 public class EscapeCharacter {
 	public static void main(String[] args) {
-		// System.out.println("转义字符的学习");
-		// System.out.println("你\t我\n他");
-		// System.out.println("你\\我\\\\他");
-		// System.out.println("你:\"我他\"");
-		// System.out.println("你:'我他'");
-		// System.out.println("你\r\n我他");
-		// System.out.println("书名\t作者\t价格\t销量");
-		// System.out.println("三国\t罗贯中\t120\t1000")
+		System.out.println("转义字符的学习");
+		System.out.println("你\t我\n他");
+		System.out.println("你\\我\\\\他");
+		System.out.println("你:\"我他\"");
+		System.out.println("你:'我他'");
+		System.out.println("你\r\n我他");
+		System.out.println("书名\t作者\t价格\t销量");
+		System.out.println("三国\t罗贯中\t120\t1000")
 		double a = 4 % (-7);
 		System.out.println(a);
 	}
