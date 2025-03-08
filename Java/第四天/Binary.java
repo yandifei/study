75a4ff2ce8d77ba90874053c35daa6ff60198edf
yandifei <3058439878@qq.com>
2025-03-09 00:42:01 +0800

    if学习

---
+++ Java/第四天/Binary.java
@@ -0,0 +1,14 @@
+//演示四种进制
+public class Binary {
+	public static void main(String[] agrs) {
+		int n1 = 0b1010;
+		int n2 = 1010;
+		int n3 = 01010;
+		int n4 = 0x1010;
+		System.out.println("n1="+n1+" n2="+n2+" n3="+n3+" n4="+n4);
+		// 123 = 0b1111011
+		// 678 = 01246
+		// 8912 = 0x22D0
+
+	}
+}
