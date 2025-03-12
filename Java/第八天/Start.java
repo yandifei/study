f4458a99f17473b2ee4d43513e2ae81ce3274405
yandifei <3058439878@qq.com>
2025-03-12 23:51:18 +0800

    java_study

---
+++ Java/第八天/Start.java
@@ -0,0 +1,15 @@
+/*Start.java --多重循环控制*/
+public class Start {
+	public static void main(String[] agrs){
+		//打印金字塔
+		//1行1个*
+		//2行3个*
+		//3行5个*
+		//3行空2、1
+		//  *
+		// ***
+		//*****
+		
+
+	}
+}
