e210027c504ca8739280862f2e9f8b3670eb9523
yandifei <3058439878@qq.com>
2025-03-13 12:25:52 +0800

    java_study

--- Java/第八天/Start.java
+++ Java/第八天/Start.java
@@ -6,10 +6,30 @@ public class Start {
 		//2行3个*
 		//3行5个*
 		//3行空2、1
-		//  *
-		// ***
-		//*****
-		
+		//  *		2 * 1 -1
+		// ***		2 * 2 -1
+		//*****		2 * 3 -1
+		/*1. 打印矩阵
+		2. 打印半个金字塔
+		3. 打印空心金字塔
+		4. 空心金字塔
+		*/
+		int level = 50;	//层级
+		for(int i = 1; i <= level; i++) {  //i表示层数
+			//输出空格
+			for(int k = 1; k <= level - i; k++) {
+				System.out.print(" ");
+			}
+			//控制每层的*个数
+			for(int j = 1; j <=2 * i - 1; j++) {
+				if(j == 1|| j == 2 * i - 1 || i == level) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 
 	}
 }
