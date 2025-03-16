a090de6e2252ad5f17acb061046734c17cb39bf5
yandifei <3058439878@qq.com>
2025-03-16 23:55:32 +0800

    java_study

---
+++ Java/第十二天/Array3.java
@@ -0,0 +1,14 @@
+/*Array3.java -- 数组学习*/
+import java.util.Scanner;
+public class Array3 {
+    public static void main(String[] agrs) {
+        short[] arr1 = new short[3];    //创建一个可以放3个元素的
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+    }
+}
+
+
+
+
