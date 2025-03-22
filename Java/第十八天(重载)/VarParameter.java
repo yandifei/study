6cc1a13f1b0c3728f21e388c1f0a81c9c7e655d0
yandifei <3058439878@qq.com>
2025-03-22 13:33:29 +0800

    java_study

---
+++ Java/第十八天(重载)/VarParameter.java
@@ -0,0 +1,41 @@
+/*VarParameter.java -- 可变参数*/
+public class VarParameter {
+	public static void main(String[] args) {
+		HspMethod m = new HspMethod();
+		System.out.println(m.sum(1,5,100));
+		int[] a = {1, 5, 100};
+		System.out.println(m.sum(a));
+		System.out.println(m.asdf(1));
+
+	}
+}
+
+class HspMethod {
+	//可以计算 2个数的和，3个数的和，4。5,
+	//可以使用方法重载
+	// public int sum(int n1, int n2) {//2个数的和
+	// 	return n1 + n2;
+	// }
+	// public int sum(int n1,int n2,int n3) {//3个数的和
+	// 	return n1 + n2 + n3;
+	// }
+	// public int sum(int n1,int n2, int n3,int n4) {//4个数的和
+	// 	return n1 + n2 + n3 + n4;
+	// }
+	//上面的三个方法名称相同，功能相同，参数个数不同->使用可变参数优化
+	//1.int...表示接受的是可变参数,类型是int即可以接收多个int(0-多)
+	//2.使用可变参数时可，可以当做数组来使用 即nums可以当作数组来使用
+	//遍历nums求和
+	public int sum(int... nums) {
+		System.out.println("接收的参数个数=" + nums.length);
+		int res = 0;
+		for(int i = 0; i < nums.length; i++) {
+			res += nums[i];
+		}
+		return res;
+	}
+
+	public int asdf(int num1, int num2 = num1) {
+		return num1 + num1;
+	}
+}
