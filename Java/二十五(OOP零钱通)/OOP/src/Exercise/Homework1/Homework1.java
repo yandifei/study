4c3a71ee858c127a7b5b32eeb61cca3070c81943
yandifei <3058439878@qq.com>
2025-04-01 23:59:38 +0800

    java

---
+++ Java/二十五(OOP零钱通)/OOP/src/Exercise/Homework1/Homework1.java
@@ -0,0 +1,6 @@
+package Exercise.Homework1;
+
+public class Homework1 {
+    //定义一个Person类{name,age,job},初始化Person 对象数组，有3个person对象，并
+    // 按照age从大到小进行排序，提示，使用冒泡排序Homework01java
+}
---
+++ Java/二十五(OOP零钱通)/OOP/src/Exercise/Homework1/Person.java
@@ -0,0 +1,9 @@
+package Exercise.Homework1;
+
+public class Person {
+//    定义一个Person类{name,age,job}
+    private String name;
+    private int age;
+    private String job;
+    
+}
