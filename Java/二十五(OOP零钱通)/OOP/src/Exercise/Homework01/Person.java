c0df46d33e50a3cc5221fcc7ddcd632a0432e5af
yandifei <3058439878@qq.com>
2025-04-02 15:04:01 +0800

    java_study

---
+++ Java/二十五(OOP零钱通)/OOP/src/Exercise/Homework01/Person.java
@@ -0,0 +1,38 @@
+package Exercise.Homework01;
+
+public class Person {
+//    定义一个Person类{name,age,job}
+    private String name;
+    private int age;
+    private String job;
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
