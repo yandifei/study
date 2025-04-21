e516506fcd4a16492dde9fc1b2a063cce9e04f0d
yandifei <3058439878@qq.com>
2025-04-22 00:02:27 +0800

    qq

--- QQ机器人/测试.py
+++ QQ机器人/测试.py
@@ -1,13 +1,16 @@
-from QQScript.QQMessageMonitor import * # 导包
+# from QQScript.QQMessageMonitor import * # 导包
+#
+#
+#
+# chat1 = QQMessageMonitor("Free my WW", "雁低飞")
+# chat1.move(0, 1000)     # 把窗口移动到指定位置0
+# chat1.create_directory(None,True)  # 如果没有转义这里会报警告，不用管
+# chat1.create_txt()  # 创建文本文件
+# print(chat1.visible_group_bulletin) # 打印群公告
+# # chat1.split_messages()
+# chat1.cancel_top_win()  # 取消窗口置顶
+# # chat2 = QQMessageMonitor("蓝宝","雁低飞")
+# # chat2.cancel_top_win()
 
-
-
-chat1 = QQMessageMonitor("Free my WW", "雁低飞")
-chat1.move(0, 1000)     # 把窗口移动到指定位置0
-chat1.create_directory(None,True)  # 如果没有转义这里会报警告，不用管
-chat1.create_txt()  # 创建文本文件
-print(chat1.visible_group_bulletin) # 打印群公告
-# chat1.split_messages()
-chat1.cancel_top_win()  # 取消窗口置顶
-# chat2 = QQMessageMonitor("蓝宝","雁低飞")
-# chat2.cancel_top_win()
+for i in range(-5,0):
+    print(i)
