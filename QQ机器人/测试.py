2e1515982b015d978466b308d819b2cf9436fb1f
yandifei <3058439878@qq.com>
2025-04-24 00:45:07 +0800

    qq

--- QQ机器人/测试.py
+++ QQ机器人/测试.py
@@ -1,11 +1,12 @@
 from QQScript.QQMessageMonitor import * # 导包
 # qq_group_name = input("请输入监听的群聊:\n") # 猫猫       雁低飞
 # qq_monitor_name = input("请输入你的身份(最好是聊天对象的名称，如你在q群的名称):\n")
-qq_group_name = "猫猫"
+qq_group_name = "鸣潮自动刷声骸"
 qq_monitor_name = "雁低飞"
 chat_win1 = QQMessageMonitor(qq_group_name, qq_monitor_name)
-chat_win1.move(0,1010)  # 把窗口移动到最上角
-print("窗口已放置最左上角，可通过鼠标拖拽拉伸")
+chat_win1.move()  # 把窗口移动到最上角 0,1010
+chat_win1.top_win()
+print("窗口已放置最左上角并置顶，可通过鼠标拖拽拉伸1")
 print(f"数据存放路径:\t{chat_win1.message_data_txt}")
 for one_message in chat_win1.message_list:  # 打印初次绑定后的消息
     print(one_message)
