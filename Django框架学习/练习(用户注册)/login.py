3b0c6474ad611900adaa02aff8c5042f50304ee6
yandifei <3058439878@qq.com>
2025-08-24 18:27:49 +0800

    用户注册

--- Django框架学习/练习(用户注册)/login.py
+++ Django框架学习/练习(用户注册)/login.py
@@ -9,26 +9,42 @@ def home():
 
 
 # 用户注册
-@app.route("/register" ,methods=["GET"])
+@app.route("/register" ,methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        # 执行注册（post方式）
+        # 1.接收用户通过P0ST形式发送过来的数据
+        print(f"所有提交的内容：{request.form}")
+        user = request.form.get("user_name")
+        pwd = request.form.get("password")
+        gender = request.form.get("gender")
+        hobby_list = request.form.getlist("hobby")
+        city = request.form.get("city")
+        skill_list = request.form.getlist("skill")
+        more = request.form.get("more")
+        print(user, pwd, gender, hobby_list, city, skill_list, more)
+        # 将用户信息写入文件中实现注册、写入到excel中实现注册、写入数据库中实现注册
 
-# 执行注册
-@app.route("/do/reg" ,methods=["GET"])
-def do_register():
-    # 1.接收用户通过GET形式发送过来的数据
-    print(request.args)
-    # 2.给用户再返回结果
-    return "注册成功"
+        # 2.给用户再返回结果
+        return "注册成功"
 
-# 执行注册（post方式）
-@app.route("/post/reg" ,methods=["POST"])
-def post_register():
-    # 1.接收用户通过P0ST形式发送过来的数据
-    print(request.form)
-    # 2.给用户再返回结果
-    return "注册成功"
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        print(request.form)
+        user_name = request.form.get("user_name")
+        password = request.form.get("password")
+        print(user_name, password)
+        return "登陆成功"
 
+
+@app.route("/index",methods=["GET"])
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
