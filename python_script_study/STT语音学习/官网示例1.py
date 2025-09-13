fe6ee37450adb4c92b4074997929264e981b85dd
yandifei <3058439878@qq.com>
2025-09-14 01:30:25 +0800

    study

--- python_script_study/STT语音学习/官网示例1.py
+++
@@ -1,9 +0,0 @@
-"""conda install ffmpeg -C conda-forge
-
-"""
-
-import whisper
-
-model = whisper.load_model("medium")
-result = model.transcribe(r"B:\study\python_script_study\STT语音学习\voice\1.ogg")
-print(result["text"])
---
+++ python_script_study/STT语音学习/测试.py
@@ -0,0 +1,33 @@
+import pyaudio
+import wave     # WAV文件操作库
+# 创建音频对象
+audio = pyaudio.PyAudio()
+# 打开音频流
+stream = audio.open(format=pyaudio.paInt16, # 16位整数格式
+                channels=1,                 # 单声道
+                rate=16000,                 # Silero-VAD 推荐的采样率（16kHz）
+                input=True,
+                frames_per_buffer=1024)     # 每次从麦克风读取的音频帧数为1024
+frames = []  # 创建一个空列表来存储音频数据（frames_per_buffer的数据）
+print("正在监听...请开始说话。按 Ctrl+C 手动停止。")
+
+# 公式：总采样数 / 每次读取量 = 16000/1024 ≈ 15.6次 → 取整16次
+for i in range(0, int(16000 / 1024 * 6)):
+    data = stream.read(1024)    # 读取一块音频数据
+    frames.append(data)         # 把数据添加到列表中
+
+print("录制完成")
+
+# 停止录音
+stream.stop_stream()  # 停止音频流
+stream.close()        # 关闭音频流
+audio.terminate()     # 关闭音频系统
+
+# 写入文件
+with wave.open("./voice/output.wav", 'wb') as wf:  # 'wb' = 写入二进制模式
+    wf.setnchannels(1)                                          # 设置声道数
+    wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))     # 设置采样宽度
+    wf.setframerate(16000)                                      # 设置采样率
+    wf.writeframes(b''.join(frames))                            # 将所有数据块合并并写入文件
+
+
