c47f19e158be1b98c3b07517a1e0176da0cc9e5c
yandifei <3058439878@qq.com>
2026-02-02 20:28:51 +0800

    study

---
+++ web前端开发学习(html_css_js)/vue3框架学习/第3天/13. 回顾TS中的_接口_泛型_自定义类型/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2 ref="title2">北京</h2>
+  <button @click="showLog">点我输出h2</button>
+  <Person ref="ren" />
+</template>
+<!-- 
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script> -->
+
+<script lang="ts" setup>
+import Person from './components/Person.vue';
+
+// 开始干活展示
+import { ref } from 'vue'
+let title2 = ref()
+
+function showLog() {
+  console.log(title2.value)
+}
+</script>
