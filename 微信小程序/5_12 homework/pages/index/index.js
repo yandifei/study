482062cdd106c347e42eb91f0a0ac9479a311ae9
yandifei <3058439878@qq.com>
2026-05-16 00:27:49 +0800

    随机图片网络请求改良版

--- 微信小程序/5_12 homework/pages/index/index.js
+++ 微信小程序/5_12 homework/pages/index/index.js
@@ -3,35 +3,46 @@ Page({
   data: {
     img_urls: [],
   },
+
   // 页面加载时立即请求图片
   onLoad() {
+    wx.showLoading({ title: '图片刷新中' });
     this.get_img_urls()
+    setTimeout(() => { wx.hideLoading() }, 1000);
   },
 
   // 页面下拉刷新
   onPullDownRefresh() {
+    wx.showLoading({ title: '图片刷新中' });
     this.get_img_urls()
-    // 至少1秒时间停顿（不然看不到load界面）
+    setTimeout(() => { wx.hideLoading(); wx.stopPullDownRefresh() }, 1000);
   },
 
   //页面触底事件的处理函数
   onReachBottom() {
     this.get_img_urls()
   },
+
+  // // 当 scroll-view 滚动时会触发这个函数
+  // onScrollView(e) {
+  //   // 如果当前界面还剩5页我就提前请求
+  //   if(e.detail.scrollTop % 10 >= 5) {
+  //     this.get_img_urls()
+  //   }
+  //   // 我靠，坑爹呀！这真不行
+  //   // console.log("当前滚动距离：", e.detail.scrollTop);
+  // },
   
   get_img_urls() {
-    // 显示加载框
-    wx.showLoading({ title: '图片刷新中' });
+    console.log("进行了请求");
     // 微信网络请求的方法
     wx.request({
       // 请求10张PC图片
       url: "https://t.alcy.cc/json?pc=10",
       // 请求成功
       success: (res) => {
-        // 设置图片链接列表
-        this.setData({ img_urls: res.data.links });
-        // 1秒时间后停止刷新用到的UI组件
-        setTimeout(() => { wx.hideLoading(); wx.stopPullDownRefresh() }, 1000);
+        // 不断增加
+        this.setData({ img_urls: [...this.data.img_urls, ...res.data.links]});
       },
       // 请求失败
       fail: (err) => {
